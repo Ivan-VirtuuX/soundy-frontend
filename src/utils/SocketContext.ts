077c6b9db6f268ce072575385d48c43c17733215
VirtuuX <somargame22@gmail.com>
2023-10-31 22:05:39 +0600

    changed socket io server url

--- src/utils/SocketContext.ts
+++ src/utils/SocketContext.ts
@@ -2,6 +2,6 @@ import { createContext } from "react";
 
 import { io } from "socket.io-client";
 
-export const socket = io("https://soundy-backend-production.up.railway.app");
+export const socket = io("https://byzantium-lovebird-veil.cyclic.app");
 
 export const SocketContext = createContext(socket);
